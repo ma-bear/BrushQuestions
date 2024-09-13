e572031a05c854c902da4c1035953b359496e2e1
葫芦给 <2992374334@qq.com>
2024-09-13 17:40:31 +0800

    后端核心业务流程开发

---
+++ mianshiya-next-backend/src/main/java/com/xq/mianshiya/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.xq.mianshiya.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xq.mianshiya.model.entity.User;
+
+/**
+* @author 29923
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2024-08-27 00:29:34
+* @Entity generator.domain.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
